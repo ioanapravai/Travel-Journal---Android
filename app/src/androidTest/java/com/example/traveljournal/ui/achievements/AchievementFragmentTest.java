b3db08fdc27109b44f0ca3cf0682c7b4504403e0
ioanapravai <ioana.pravai99@gmail.com>
2020-06-11 20:42:13 +0300

    ui tests

---
+++ app/src/androidTest/java/com/example/traveljournal/ui/achievements/AchievementFragmentTest.java
@@ -0,0 +1,17 @@
+package com.example.traveljournal.ui.achievements;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class AchievementFragmentTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
